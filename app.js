2e031fe225c3e6c9e46fabfe54752e762ab9a579
PranjalBuilds <imcae22060@gmail.com>
2024-11-01 19:03:42 +0530

    understanding the basics

---
+++ app.js
@@ -0,0 +1,4 @@
+// here we are installing npm package 'cat-me' for understanding purpose, when we run the code it gives a cat drawn by some characters or symbols in terminal 
+
+const catMe = require('cat-me');
+console.log(catMe());
