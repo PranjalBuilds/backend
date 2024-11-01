3a46cb0ecf9af1cc220c78156dbcaef17e86401f
PranjalBuilds <imcae22060@gmail.com>
2024-11-01 20:26:25 +0530

    routing (basics)

--- app.js
+++ app.js
@@ -6,8 +6,17 @@ const http = require('http')
 //this creates an instance of http server
 
 const server = http.createServer((req,res)=>{
-    console.log(req.url);
-    res.end('Hello World!') 
+   if(req.url == "/about") {
+        res.end("Welcome to About Page!");
+   } 
+
+   if(req.url == "/profile"){
+        res.end('Welcome to the Profile Page!');
+   }
+
+   if(req.url == "/") {
+        res.end("Hello World!")
+   }
 })
 
 server.listen(3000)
