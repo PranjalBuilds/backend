2d38da753a53080881db0bc3f96a7b1acbf85106
PranjalBuilds <imcae22060@gmail.com>
2024-11-01 21:17:58 +0530

    Middleware

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
-//Working with Express.JS x EJS
+//Middlewares
+
+// middlewares are basically used to pass all routes by a functions that we create which has some sort of logic behind it.
+
+// types of middlewares: 
+//built-in, custom, third-party.
 
 const express = require('express');
 const app = express(); 
 
-//telling to Express Js that which view engine we are working with. 
-
 app.set("view engine", 'ejs');
+
+
+//simple example of custom middleware.
+
+app.use((req, res, next)=> {
+    console.log('This is middleware');    
+    
+    let a = 1;
+    let b = 3;
+    let c = a + b;
+
+    console.log(c);
+
+    return next();
+});
 
 app.get('/', (req, res) => {
     res.render('index');
